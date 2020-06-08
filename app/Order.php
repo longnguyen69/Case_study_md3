0ff26af5e984c98d4d11d1e2e9b9fe0b33150098
longnguyen69 <nguyenthanhlongtlck@gmail.com>
2020-06-08 09:09:50 +0700

    Test Project

---
+++ app/Order.php
@@ -0,0 +1,17 @@
+<?php
+
+
+namespace App;
+
+
+use Illuminate\Database\Eloquent\Model;
+
+class Order extends Model
+{
+    public $timestamps = false;
+
+    protected $table = 'orders';
+    protected $fillable = ['name' ,'order_Note','phone','address','account_id','status' ];
+    //123ac
+
+}
